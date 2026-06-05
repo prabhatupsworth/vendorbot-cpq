66f61b657ff956a234d4f5eac7cdab649e052970
prabhatupsworth <prabhat@upsworth.com>
2026-06-05 10:43:35 +0530

    changes done

--- Modules/Supplier/routes/web.php
+++ Modules/Supplier/routes/web.php
@@ -2,13 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 use Modules\Supplier\Http\Controllers\SupplierController;
-Route::get('suppliers/import', [SupplierController::class, 'importSuppliers'])->name('suppliers.import');
 
-Route::get('suppliers', [SupplierController::class, 'index'])->name('suppliers.index');
-Route::get('suppliers/create', [SupplierController::class, 'create'])->name('suppliers.create');
-Route::post('suppliers', [SupplierController::class, 'store'])->name('suppliers.store');
-Route::get('suppliers/{id}', [SupplierController::class, 'show'])->name('suppliers.show');
-Route::get('suppliers/{id}/edit', [SupplierController::class, 'edit'])->name('suppliers.edit');
-Route::put('suppliers/{id}', [SupplierController::class, 'update'])->name('suppliers.update');
-Route::delete('suppliers/{id}', [SupplierController::class, 'destroy'])->name('suppliers.destroy');
+Route::middleware(['web', 'auth'])->group(function () {
+    Route::get('suppliers/import', [SupplierController::class, 'importSuppliers'])->name('suppliers.import');
 
+    Route::get('suppliers', [SupplierController::class, 'index'])->name('suppliers.index');
+    Route::get('suppliers/create', [SupplierController::class, 'create'])->name('suppliers.create');
+    Route::post('suppliers', [SupplierController::class, 'store'])->name('suppliers.store');
+    Route::get('suppliers/{id}', [SupplierController::class, 'show'])->name('suppliers.show');
+    Route::get('suppliers/{id}/edit', [SupplierController::class, 'edit'])->name('suppliers.edit');
+    Route::put('suppliers/{id}', [SupplierController::class, 'update'])->name('suppliers.update');
+    Route::delete('suppliers/{id}', [SupplierController::class, 'destroy'])->name('suppliers.destroy');
+});
